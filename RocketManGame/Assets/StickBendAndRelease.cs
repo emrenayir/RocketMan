739afb85a9bf9603db24d51460d117a6a736f2fc
emrenayir <emrenayir49@gmail.com>
2021-05-08 12:30:45 +0300

    blend tree for stick

---
+++ RocketManGame/Assets/StickBendAndRelease.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickBendAndRelease : MonoBehaviour
+{
+    private Animator stickAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        stickAnimator = this.gameObject.GetComponent<Animator>();
+        stickAnimator.SetFloat("StickBender",0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            stickAnimator.SetFloat("StickBender",1f);
+        }
+    }
+}

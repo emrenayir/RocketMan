0a6ae71ff3932c5f4f758705c125b8abaec80f2e
emrenayir <emrenayir49@gmail.com>
2021-05-08 14:02:41 +0300

    Update StickBendAndRelease.cs

--- RocketManGame/Assets/StickBendAndRelease.cs
+++ RocketManGame/Assets/StickBendAndRelease.cs
@@ -5,44 +5,53 @@ using UnityEngine;
 public class StickBendAndRelease : MonoBehaviour
 {
     private Animator stickAnimator;
-    
+    [SerializeField] private bool isWorking;
     [SerializeField] private float screenWithEffectInAngles=120;
     [SerializeField] private float pixelDivider;
     [SerializeField] private float currentPixelPosHorizontal;
     [SerializeField] private float pixelStartPosHorizontal;
     [SerializeField] private bool startActive;
+    
+    
+    private static readonly int Start1 = Animator.StringToHash("Start");
+    private static readonly int StickBender = Animator.StringToHash("StickBender");
+
+    public StickBendAndRelease()
+    {
+        isWorking = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         pixelDivider = Screen.width / screenWithEffectInAngles;
         stickAnimator = this.gameObject.GetComponent<Animator>();
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !startActive)
+        if (isWorking)
         {
-            stickAnimator.SetBool("Start",true);
-            startActive = true;
-            pixelStartPosHorizontal = Input.mousePosition.x;
-                
-            
-        }
-        else if (Input.GetMouseButton(0) && startActive)
-        {
-            currentPixelPosHorizontal = Input.mousePosition.x;
-            float pixelOffset = (currentPixelPosHorizontal - pixelStartPosHorizontal)/1000;
-            pixelOffset =  Mathf.Clamp(pixelOffset, -1f, 0f);
-            Debug.Log(pixelOffset);
-            stickAnimator.SetFloat("StickBender",-pixelOffset);
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            stickAnimator.SetBool("Start",false);
-            Destroy(this);
+            if (Input.GetMouseButton(0) && !startActive)
+            {
+                stickAnimator.SetBool(Start1,true);
+                startActive = true;
+                pixelStartPosHorizontal = Input.mousePosition.x;
+            }
+            else if (Input.GetMouseButton(0) && startActive)
+            {
+                currentPixelPosHorizontal = Input.mousePosition.x;
+                float pixelOffset = (currentPixelPosHorizontal - pixelStartPosHorizontal)/1000;
+                pixelOffset =  Mathf.Clamp(pixelOffset, -1f, 0f);
+                Debug.Log(pixelOffset);
+                stickAnimator.SetFloat(StickBender,-pixelOffset);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                stickAnimator.SetBool(Start1,false);
+                isWorking = false;
+            }
         }
     }
 }

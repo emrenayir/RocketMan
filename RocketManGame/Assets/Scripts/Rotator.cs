74eb43e8647cbf83b04c44fe63d4dc327846af80
emrenayir <emrenayir49@gmail.com>
2021-05-13 00:55:31 +0300

    Proje sonu
    - Kullanılmayan assetler kaldırıldı.
    - Kanat hareketleriyle ilgili değişiklikler yapıldı.
    -Mobilde denendi.
    -Fps sayacı konuldu.

---
+++ RocketManGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public bool work;
+
+    public bool flag = true;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (work)
+        {
+          this.transform.Rotate(new Vector3(25,0,0));
+        }
+        else if (flag)
+        {
+                Vector3 temp = transform.rotation.eulerAngles;
+                temp.x = 100.0f;
+                transform.rotation = Quaternion.Euler(temp);
+                flag = false;
+        }
+        
+    }
+}
